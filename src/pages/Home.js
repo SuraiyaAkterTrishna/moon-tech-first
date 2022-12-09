fd1e8864e349657218e3c23a467befb3cc6b26c6
Suraiya  Akter Trishna <suraiyaaktertrishna@gmail.com>
2022-12-09 14:24:32 +0600

    final commit

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ProductCard from "../components/ProductCard";
+import { useProduct } from "../context/ProductProvider";
+
+const Home = () => {
+  const {state : {products, loading, error}} = useProduct();
+  let content;
+  if(loading){
+    content = <p>Loading............................Wait Few Moment</p>
+  };
+  if(error){
+    content = <p>Danger............................Something went wrong</p>
+  };
+  if(!loading && !error && products.length === 0){
+    content = <p>Nothing to show............................Products is Empty</p>
+  };
+  if(!loading && !error && products.length){
+    content = products.map(product => <ProductCard key={product.id} product={product} />)
+  };
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+      {content}
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/state/ProductState/productReducer.js
@@ -0,0 +1,51 @@
+import { actionTypes } from "./actionTypes"
+export const initialState = {
+  loading : false,
+  products : [],
+  error : false,
+  cart : [],
+  wishlist : [],
+}
+export const productReducer = (state, action) => {
+  switch (action.type) {
+    case actionTypes.FETCHING_START:
+      return {
+        ...state,
+        loading : true,
+        error : false,
+      };
+    case actionTypes.FETCHING_SUCCESS:
+      return {
+        ...state,
+        loading : false,
+        products : action.payload,
+        error : false,
+      };
+    case actionTypes.FETCHING_ERROR:
+      return {
+        ...state,
+        loading : false,
+        error : true,
+      };
+    case actionTypes.ADD_TO_CART:
+      return {
+        ...state,
+        cart : [...state.cart, action.payload],
+      };
+    case actionTypes.ADD_TO_WISHLIST:
+      return {
+        ...state,
+        wishlist : [...state.wishlist, action.payload],
+      };
+    case actionTypes.REMOVE:
+      return {
+        ...state,
+        products : [...state.products.filter( product => product !== action.payload)],
+        cart : [...state.cart.filter( product => product !== action.payload)],
+        wishlist : [...state.wishlist.filter( product => product !== action.payload)],
+      };
+  
+    default:
+      return state;
+  }
+}
